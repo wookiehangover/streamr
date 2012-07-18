3a7bc56646e9a354b4d10e7f710e2e54cceb26f8
Alon Burg <burgalon@gmail.com>
2012-07-18 15:09:39 -0400

    writeable ==> writable

--- knox-stream.js
+++ knox-stream.js
@@ -11,7 +11,7 @@ var util = require('util');
 
 function WriteStream( path, file ){
   stream.Stream.call(this);
-  this.writeable = true;
+  this.writable = true;
 
   client.putStream( this, file, function( err, res ){
     console.log(res.statusCode);
